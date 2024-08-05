016c40957d3db6070d9e44594e55462a35fbcbb6
Eri Yawata <yawata.eri@gmail.com>
2024-08-05 13:49:43 +0900

    Commit some changes for testing

---
+++ test/system/questions_test.rb
@@ -0,0 +1,8 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "p", text: "Ask your coach anything"
+  end
+end
